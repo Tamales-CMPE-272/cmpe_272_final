8290fce61ebbeb295f9a1177a84dfd4672c0d7bb
Joel Zapana <joel.zapana@sjsu.edu>
2025-05-01 21:08:42 -0700

    Edited backend (title repo) to choose latest title of employee when gettinng profile details of employee

--- backend/tamalesHr/src/main/java/com/sjsu/cmpe272/tamales/tamalesHr/repository/TitleRepository.java
+++ backend/tamalesHr/src/main/java/com/sjsu/cmpe272/tamales/tamalesHr/repository/TitleRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface TitleRepository extends JpaRepository<Title, Long> {
 
-    @Query("SELECT t FROM Title t WHERE t.id.emp_no = :emp_no ORDER BY t.id.from_date DESC")
-    Optional<Title> findByEmpNo(@Param("emp_no") Integer emp_no);
+    @Query("SELECT t FROM Title t WHERE t.id.emp_no = :emp_no ORDER BY t.to_date DESC")
+    List<Title> findByEmpNoOrderByToDateDesc(@Param("emp_no") Integer emp_no);
 }
